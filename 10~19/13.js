2a65c540a6fb384bb56024b20a71fac2ef318b4e
Grapefruitgreentealoe <76976331+Grapefruitgreentealoe@users.noreply.github.com>
2021-05-26 21:48:09 +0900

    Add files via upload

---
+++ 10~19/13.js
@@ -0,0 +1,12 @@
+function switch_size(str) {
+    let ret = "";
+    for (x of str) {
+        if (x => 'a' && x <= 'z') ret += x.toUpperCase(); //if(x==x.toLowerCase());
+        else ret += x.toLowerCase();
+    }
+
+    return ret;
+}
+
+str = "HeLlO";
+console.log(switch_size(str));

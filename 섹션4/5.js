45ea77a2b7c121486fa4411b37527451b85287b6
Grapefruitgreentealoe <76976331+Grapefruitgreentealoe@users.noreply.github.com>
2021-06-04 18:37:03 +0900

    Add files via upload

---
+++ 섹션4/5.js
@@ -0,0 +1,31 @@
+function max_num(pick, arr) {
+    let n = arr.length;
+    let com_arr = [];
+    let ret = [];
+    for (i = 0; i < n - 2; i++) {
+        for (j = 1; j + i < n - 1; j++) {
+            for (k = 1; k + j + i < n; k++) {
+
+                com_arr.push(arr[i] + arr[i + j] + arr[i + j + k]);
+                //console.log(arr[i], arr[i + j], arr[i + j + k]);
+
+
+            }
+        }
+    }
+    com_arr.sort(function(a, b) {
+        return b - a;
+    });
+    let unique = com_arr.filter((item, pos) => com_arr.indexOf(item) === pos);
+    //console.log(unique);
+    for (i = 0; i < pick; i++) {
+        ret.push(Math.max.apply(null, unique));
+        unique.shift();
+        //console.log(unique[0]);
+    }
+    return ret[pick - 1];
+}
+
+pick = 3;
+arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
+console.log(max_num(pick, arr));
